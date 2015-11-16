1fc68f595c5a6dcb076dd3966dfcb314d325aa3c
Menushka <menushka@savewithsprout.com>
2015-11-15 22:32:04 -0500

    Created start menu - Used self created logo - Used temp background image - Colors need to be updated

--- app/src/main/java/savewithsprout/unicornapp/MainActivity.java
+++ app/src/main/java/savewithsprout/unicornapp/MainActivity.java
@@ -2,6 +2,10 @@ package savewithsprout.unicornapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -10,14 +14,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void signIn(View view){
-
-    }
-
-    public void createAccount(View view){
-
     }
 }
---
+++ app/src/main/java/savewithsprout/unicornapp/StartActivity.java
@@ -0,0 +1,23 @@
+package savewithsprout.unicornapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+
+    public void signIn(View view){
+
+    }
+
+    public void createAccount(View view){
+
+    }
+}
